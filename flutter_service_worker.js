653ea1475d2857e261fe6137043dc3f5cf8df0d0
tguerin <tguerin@users.noreply.github.com>
2025-06-02 12:59:43 +0000

    deploy: 2aa18fe89e3281994c0987551bf62d70b1a7072b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2e
 "canvaskit/chromium/canvaskit.js": "8191e843020c832c9cf8852a4b909d4c",
 "canvaskit/chromium/canvaskit.wasm": "c054c2c892172308ca5a0bd1d7a7754b",
 "canvaskit/chromium/canvaskit.js.symbols": "f7c5e5502d577306fb6d530b1864ff86",
-"flutter_bootstrap.js": "06450ea278b6370f4cc2cab4cf9e5e79",
+"flutter_bootstrap.js": "97d741528a771b0964633acde2a243ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "751d64dfc80980d00eb537fe8bead2c1",
 "/": "751d64dfc80980d00eb537fe8bead2c1",
